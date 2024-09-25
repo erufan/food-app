cbbd3c2fdc0e8e01d10769f950096032b667d795
erfan taheri <erfanta197@gmail.com>
2024-09-26 00:13:24 +0330

    add primary skeletons and structure for meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,25 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealItem from "@/components/meals/meal-item";
+
 const MealsPage = () => {
-  return <div>MealsPage</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Pick your favorite recipe and turn cooking into a fun adventure!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealItem meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
