564a8aba5954f7343df3ab415e7b3750b3733d45
erfan taheri <erfanta197@gmail.com>
2024-09-28 17:45:50 +0330

    add POST endpoint to post one meal from share meal page

--- app/api/meals/route.js
+++ app/api/meals/route.js
@@ -16,3 +16,24 @@ export async function GET(request) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
+
+export async function POST(request) {
+  const { slug, title, image, summary, instructions, name, email } =
+    await request.json();
+
+  try {
+    await client.query("BEGIN");
+    const queryText = `
+      INSERT INTO meals (slug, title, image, summary, instructions, creator, creator_email)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *;
+    `;
+    const values = [slug, title, image, summary, instructions, name, email];
+    await client.query(queryText, values);
+    await client.query("COMMIT");
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.error("Error inserting meal:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
